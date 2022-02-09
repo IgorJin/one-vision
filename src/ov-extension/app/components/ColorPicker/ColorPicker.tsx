fe34e6dbb6a250dcee80af4adb65c02771ba6677
Igor <igor.zubenko@1win.pro>
2022-02-09 22:29:07 +0300

    add colorpicker

--- src/ov-extension/app/components/ColorPicker/ColorPicker.tsx
+++ src/ov-extension/app/components/ColorPicker/ColorPicker.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { Popover } from '../index'
 import "./ColorPicker.scss"
 
 interface ColorPickerProps {
-  size: number;
+  color: string;
 }
 
-export const ColorPicker: React.FC<ColorPickerProps> = ({ size }) => {
+const ColorInput = ({ color } : any)  => {
   return (
-    <div className={`color-picker size-${size}`}>
-    </div>
+    <div className="color-input" style={{backgroundColor: color}}></div>
   )
 }
 
+const ColorComponent = ({ color } : any)  => {
+  return (
+    <div className="color-component">color-component</div>
+  )
+}
+
+export const ColorPicker: React.FC<ColorPickerProps> = ({ color }) => {
+  return (
+    <Popover isOpen content={ColorComponent(color)} position="top">
+      <ColorInput color={color} />
+    </Popover>
+  )
+}
